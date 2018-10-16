68144750aff59c255bb5c3daea6f7cb9f075aa82
Davidgregorio <dhavidy@gmail.com>
2018-10-16 14:16:49 -0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -14,12 +14,12 @@
 using namespace std;
 
 int main(){
-    double *menuOp;                     // Capta as opções, onde nas posições: 1- algoritmo 2-ordenação 4-tamanho da instancia
+    double *menuOp;                     // Capta as opÃ§Ãµes, onde nas posiÃ§Ãµes: 1- algoritmo 2-ordenaÃ§Ã£o 4-tamanho da instancia
     menuOp = (double *)malloc (6 *sizeof(double));
     int arq, sair =1;
     cout << ("\n     ALGORITMOS DE ORDENACAO\n\n     1 - Gerar Arquivos \n     0 - Nao Gerar \n     :");cin >> arq;
     if(arq == 1){cout << "\n Gerando arquivos ..";
-        gerarInstancias();              // Chama a função que gera os números (instancias)
+        gerarInstancias();              // Chama a funÃ§Ã£o que gera os nÃºmeros (instancias)
         }system("cls");
     do{ *menuOp = menu(menuOp);         // Loop: apresenta o menu, ate que o usuario digita 0 para sair
         if (menuOp[1] == 0) return 0;
